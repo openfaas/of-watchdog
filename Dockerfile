e84222be0b130a21492f0ec91fa08222e9daefcd
Alex Ellis <alexellis2@gmail.com>
2017-10-26 10:02:02 +0100

    CI
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM golang:1.9.1
+RUN mkdir -p /go/src/github.com/openfaas-incubator/of-watchdog
+WORKDIR /go/src/github.com/openfaas-incubator/of-watchdog
+
+COPY main.go    .
+COPY config     config
+COPY functions  functions
+
+# Run a gofmt and exclude all vendored code.
+RUN test -z "$(gofmt -l $(find . -type f -name '*.go' -not -path "./vendor/*"))"
+
+RUN go test -v ./...
+
+# Stripping via -ldflags "-s -w" 
+RUN CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-s -w" -installsuffix cgo -o of-watchdog . \
+    && CGO_ENABLED=0 GOOS=darwin go build -a -ldflags "-s -w" -installsuffix cgo -o of-watchdog-darwin . \
+    && GOARM=6 GOARCH=arm CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-s -w" -installsuffix cgo -o of-watchdog-armhf . \
+    && GOARCH=arm64 CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-s -w" -installsuffix cgo -o of-watchdog-arm64 . \
+    && GOOS=windows CGO_ENABLED=0 go build -a -ldflags "-s -w" -installsuffix cgo -o of-watchdog.exe .
---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export arch=$(uname -m)
+
+if [ ! "$arch" = "x86_64" ] ; then
+    echo "Build not supported on $arch, use cross-build."
+    exit 1
+fi
+
+if [ ! $http_proxy == "" ] 
+then
+    docker build --no-cache --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -t functions/of-watchdog:build .
+else
+    docker build -t functions/of-watchdog:build .
+fi
+
+docker create --name buildoutput functions/of-watchdog:build echo
+
+docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog ./of-watchdog
+docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog-darwin ./of-watchdog-darwin
+docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog-armhf ./of-watchdog-armhf
+docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog-arm64 ./of-watchdog-arm64
+docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog.exe ./of-watchdog.exe
+
+docker rm buildoutput
+
