50d4f2b1620b7c7ac721ce479f1b12bc03809796
Burton Rheutan <rheutan7@gmail.com>
2019-06-04 13:47:16 -0500

    Update docker images to use openfaas namespace
    Signed-off-by: Burton Rheutan <rheutan7@gmail.com>

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 if [ ! "$http_proxy" = "" ]
 then
-    docker build --no-cache --build-arg "https_proxy=$https_proxy" --build-arg "http_proxy=$http_proxy" -t functions/of-watchdog:build .
+    docker build --no-cache --build-arg "https_proxy=$https_proxy" --build-arg "http_proxy=$http_proxy" -t openfaas/of-watchdog:build .
 else
-    docker build -t functions/of-watchdog:build .
+    docker build -t openfaas/of-watchdog:build .
 fi
 
 docker build --no-cache --build-arg PLATFORM="-darwin" -t openfaas/of-watchdog:latest-dev-darwin . -f Dockerfile.packager
@@ -13,7 +13,7 @@ docker build --no-cache --build-arg PLATFORM="-arm64" -t openfaas/of-watchdog:la
 docker build --no-cache --build-arg PLATFORM=".exe" -t openfaas/of-watchdog:latest-dev-windows . -f Dockerfile.packager
 docker build --no-cache --build-arg PLATFORM="" -t openfaas/of-watchdog:latest-dev-x86_64 . -f Dockerfile.packager
 
-docker create --name buildoutput functions/of-watchdog:build echo
+docker create --name buildoutput openfaas/of-watchdog:build echo
 
 docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog ./of-watchdog
 docker cp buildoutput:/go/src/github.com/openfaas-incubator/of-watchdog/of-watchdog-darwin ./of-watchdog-darwin
