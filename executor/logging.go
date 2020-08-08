e918a3391b3dd3e424474529595482af56686e36
Lucas Roesler <roesler.lucas@gmail.com>
2020-08-08 23:23:19 +0200

    Support piping long log lines from the function
    **What**
    - Use the bufio ReadLine method so that we can check if the the log line
      has been broken into several parts due to the internal buffering.
      This implementation has been adapted from
      https://github.com/hashicorp/go-plugin/pull/98
    
      The implementation also preserves the timestamp prefix and is
      fully backwards compatible.
    
      Resolves #100
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- executor/logging.go
+++ executor/logging.go
@@ -2,23 +2,129 @@ package executor
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
+	"time"
 )
 
+var stdLogBufferSize = 64 * 1024
+
 // bindLoggingPipe spawns a goroutine for passing through logging of the given output pipe.
+//
+// This implementation is adapted from https://github.com/hashicorp/go-plugin/pull/98
 func bindLoggingPipe(name string, pipe io.Reader, output io.Writer) {
 	log.Printf("Started logging %s from function.", name)
 
-	scanner := bufio.NewScanner(pipe)
-	logger := log.New(output, log.Prefix(), log.Flags())
+	scanner := bufio.NewReaderSize(pipe, stdLogBufferSize)
 
 	go func() {
-		for scanner.Scan() {
-			logger.Printf("%s: %s", name, scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			log.Printf("Error scanning %s: %s", name, err.Error())
+		logFlags := log.Flags()
+		continuation := false
+
+		for {
+			line, isPrefix, err := scanner.ReadLine()
+			switch {
+			case err == io.EOF:
+				return
+			case err != nil:
+				log.Printf("Error scanning %s: %s", name, err.Error())
+				return
+			}
+
+			fmt.Println(isPrefix, string(line))
+
+			if !continuation {
+				// we are not continuing a previous line, so we
+				// start the log line with the log prefix
+				output.Write(logHeader(logFlags))
+			}
+
+			output.Write(line)
+			// the line is longer than stdLogBufferSize
+			if isPrefix || continuation {
+
+				// we are continuing a long log line,
+				// but it is the end of the actual line,
+				// so put the newline back in
+				if !isPrefix {
+					output.Write([]byte{'\n'})
+				}
+
+				continuation = isPrefix
+				continue
+			}
+
+			// not a prefix or continuation, so we have written a complete line
+			// so put the newline back in
+			output.Write([]byte{'\n'})
 		}
 	}()
+}
+
+// logHeader copies timestamp construction from the log package `formatHeader`
+// this is needed to preserve backwards compatibility while also allowing us
+// to control when newlines are added to the output. By default, all print
+// statements to the logger will suffix a newline (if it is missing). This
+// prevents us from writing very long lines to the output.
+//
+// Note that this does not copy the support for the following log flags
+// * Lshortfile
+// * Llongfile
+// * Lmsgprefix
+//
+// The first two don't really make sense for the of-watchdog because we
+// don't know the file location of the log statement in the actual function
+// implementation.  The second is not implemented because it was never
+// used previously.
+//
+// Supporting the flags makes the unit testing easier.
+func logHeader(flag int) []byte {
+	buf := []byte{}
+	t := time.Now()
+	if flag&(log.Ldate|log.Ltime|log.Lmicroseconds) != 0 {
+		if flag&log.LUTC != 0 {
+			t = t.UTC()
+		}
+		if flag&log.Ldate != 0 {
+			year, month, day := t.Date()
+			itoa(&buf, year, 4)
+			buf = append(buf, '/')
+			itoa(&buf, int(month), 2)
+			buf = append(buf, '/')
+			itoa(&buf, day, 2)
+			buf = append(buf, ' ')
+		}
+		if flag&(log.Ltime|log.Lmicroseconds) != 0 {
+			hour, min, sec := t.Clock()
+			itoa(&buf, hour, 2)
+			buf = append(buf, ':')
+			itoa(&buf, min, 2)
+			buf = append(buf, ':')
+			itoa(&buf, sec, 2)
+			if flag&log.Lmicroseconds != 0 {
+				buf = append(buf, '.')
+				itoa(&buf, t.Nanosecond()/1e3, 6)
+			}
+			buf = append(buf, ' ')
+		}
+	}
+	return buf
+}
+
+// Cheap integer to fixed-width decimal ASCII. Give a negative width to avoid zero-padding.
+func itoa(buf *[]byte, i int, wid int) {
+	// Assemble decimal in reverse order.
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || wid > 1 {
+		wid--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
+	}
+	// i < 10
+	b[bp] = byte('0' + i)
+	*buf = append(*buf, b[bp:]...)
 }
---
+++ executor/logging_test.go
@@ -0,0 +1,53 @@
+package executor
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestBindLoggingPipe(t *testing.T) {
+	// set the log timestamp prefix to just the date to make
+	// checking the output deterministic
+	log.SetFlags(log.Ldate)
+
+	// make the buffer small so that the test stays readable
+	orig := stdLogBufferSize
+	stdLogBufferSize = 32
+	defer func() {
+		stdLogBufferSize = orig
+		log.SetFlags(log.LstdFlags)
+	}()
+
+	// test several empty lines, a long line and a short line
+	msg := `
+this line is more than 32 bytes long
+
+this line is short
+`
+
+	reader := strings.NewReader(msg)
+
+	out := bytes.Buffer{}
+	bindLoggingPipe("TestFunc", reader, &out)
+
+	// give the pipe time to actually parse the logs
+	time.Sleep(1 * time.Second)
+
+	ouput := out.String()
+
+	now := time.Now().Format("2006/01/02 ")
+	expected := fmt.Sprintf(`%s
+%sthis line is more than 32 bytes long
+%s
+%sthis line is short
+`, now, now, now, now)
+
+	if ouput != expected {
+		t.Fatalf("incorrect log output: expected \n%q, got \n%q", expected, ouput)
+	}
+
+}
