78094524e9d6b9b324f9dbb39f9a42509c8560c6
Jeffrey C. Ollie <jeff@ocjtech.us>
2021-08-16 16:24:06 -0500

    don't pass output as a format string
    Signed-off-by: Jeffrey C. Ollie <jeff@ocjtech.us>

--- executor/logging.go
+++ executor/logging.go
@@ -33,7 +33,7 @@ func bindLoggingPipe(name string, pipe io.Reader, output io.Writer, logPrefix bo
 			if logPrefix {
 				logger.Printf("%s: %s", name, scanner.Text())
 			} else {
-				logger.Printf(scanner.Text())
+				logger.Printf("%s", scanner.Text())
 			}
 		}
 		if err := scanner.Err(); err != nil {
