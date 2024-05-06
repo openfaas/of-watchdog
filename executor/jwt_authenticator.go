a57347f0d144a7265c4f27a24d16512bf80e304b
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2024-05-06 10:33:30 +0200

    Validate auth permissions in function claims
    Compare the auth permissions in the function claim with the function
    name and namespace the verify if the provided JWT token is authorized
    to invoke the function.
    
    Signed-off-by: Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>

--- executor/jwt_authenticator.go
+++ executor/jwt_authenticator.go
@@ -8,16 +8,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/rakutentech/jwk-go/jwk"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func NewJWTAuthMiddleware(next http.Handler) (http.Handler, error) {
-
 	var authority = "http://gateway.openfaas:8080/.well-known/openid-configuration"
 	if v, ok := os.LookupEnv("jwt_auth_local"); ok && (v == "true" || v == "1") {
 		authority = "http://127.0.0.1:8000/.well-known/openid-configuration"
@@ -50,6 +50,15 @@ func NewJWTAuthMiddleware(next http.Handler) (http.Handler, error) {
 
 	issuer := config.Issuer
 
+	namespace, err := getFnNamespace()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get function namespace: %s", err)
+	}
+	name, err := getFnName()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get function name: %s", err)
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		st := time.Now()
 		for _, key := range keyset.Keys {
@@ -67,15 +76,18 @@ func NewJWTAuthMiddleware(next http.Handler) (http.Handler, error) {
 			return
 		}
 
-		mapClaims := jwt.MapClaims{}
+		parseOptions := []jwt.ParserOption{
+			jwt.WithIssuer(issuer),
+			// The OpenFaaS gateway is the expected audience but we can use the issuer url
+			// since the gateway is also the issuer of function tokens and thus has the same url.
+			jwt.WithAudience(issuer),
+			jwt.WithLeeway(time.Second * 1),
+		}
 
-		token, err := jwt.ParseWithClaims(bearer, &mapClaims, func(token *jwt.Token) (interface{}, error) {
+		functionClaims := FunctionClaims{}
+		token, err := jwt.ParseWithClaims(bearer, &functionClaims, func(token *jwt.Token) (interface{}, error) {
 			if jwtAuthDebug {
-				log.Printf("[JWT Auth] Token: audience: %v\tissuer: %v", mapClaims["aud"], mapClaims["iss"])
-			}
-
-			if mapClaims["iss"] != issuer {
-				return nil, fmt.Errorf("invalid issuer: %s", mapClaims["iss"])
+				log.Printf("[JWT Auth] Token: audience: %v\tissuer: %v", functionClaims.Audience, functionClaims.Issuer)
 			}
 
 			kid, ok := token.Header["kid"].(string)
@@ -94,7 +106,7 @@ func NewJWTAuthMiddleware(next http.Handler) (http.Handler, error) {
 				return nil, fmt.Errorf("invalid kid: %s", kid)
 			}
 			return key.Key.(crypto.PublicKey), nil
-		})
+		}, parseOptions...)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("failed to parse JWT token: %s", err), http.StatusUnauthorized)
 
@@ -106,6 +118,13 @@ func NewJWTAuthMiddleware(next http.Handler) (http.Handler, error) {
 			http.Error(w, fmt.Sprintf("invalid JWT token: %s", bearer), http.StatusUnauthorized)
 
 			log.Printf("%s %s - %d ACCESS DENIED - (%s)", r.Method, r.URL.Path, http.StatusUnauthorized, time.Since(st).Round(time.Millisecond))
+			return
+		}
+
+		if !isAuthorized(functionClaims.Authentication, namespace, name) {
+			http.Error(w, "insufficient permissions", http.StatusForbidden)
+
+			log.Printf("%s %s - %d ACCESS DENIED - (%s)", r.Method, r.URL.Path, http.StatusForbidden, time.Since(st).Round(time.Millisecond))
 			return
 		}
 
@@ -178,4 +197,88 @@ func getConfig(jwksURL string) (OpenIDConfiguration, error) {
 type OpenIDConfiguration struct {
 	Issuer  string `json:"issuer"`
 	JWKSURI string `json:"jwks_uri"`
+}
+
+func getFnName() (string, error) {
+	name, ok := os.LookupEnv("OPENFAAS_NAME")
+	if !ok || len(name) == 0 {
+		return "", fmt.Errorf("env variable 'OPENFAAS_NAME' not set")
+	}
+
+	return name, nil
+}
+
+func getFnNamespace() (string, error) {
+	nsVal, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		return "", err
+	}
+	return string(nsVal), nil
+}
+
+type FunctionClaims struct {
+	jwt.RegisteredClaims
+
+	Authentication AuthPermissions `json:"function"`
+}
+
+type AuthPermissions struct {
+	Permissions []string `json:"permissions"`
+	Audience    []string `json:"audience,omitempty"`
+}
+
+func isAuthorized(auth AuthPermissions, namespace, fn string) bool {
+	functionRef := fmt.Sprintf("%s:%s", namespace, fn)
+
+	return matchResource(auth.Audience, functionRef, false) &&
+		matchResource(auth.Permissions, functionRef, true)
+}
+
+// matchResources checks if ref matches one of the resources.
+// The function will return true if a match is found.
+// If required is false, this function will return true if a match is found or the resource list is empty.
+func matchResource(resources []string, ref string, req bool) bool {
+	if !req {
+		if len(resources) == 0 {
+			return true
+		}
+	}
+
+	for _, res := range resources {
+		if res == "*" {
+			return true
+		}
+
+		if matchString(res, ref) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func matchString(pattern string, value string) bool {
+	if len(pattern) > 0 {
+		result, _ := regexp.MatchString(wildCardToRegexp(pattern), value)
+		return result
+	}
+
+	return pattern == value
+}
+
+// wildCardToRegexp converts a wildcard pattern to a regular expression pattern.
+func wildCardToRegexp(pattern string) string {
+	var result strings.Builder
+	for i, literal := range strings.Split(pattern, "*") {
+
+		// Replace * with .*
+		if i > 0 {
+			result.WriteString(".*")
+		}
+
+		// Quote any regular expression meta characters in the
+		// literal text.
+		result.WriteString(regexp.QuoteMeta(literal))
+	}
+	return result.String()
 }
